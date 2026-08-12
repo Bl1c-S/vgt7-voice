892c8f91d0d3b05235fad745a9537cf6c49faec5
Олександр Кубарич <kuseas5@gmail.com>
2026-08-12 21:52:17 +0300

    Add `Vgt7User` model, `Vgt7UserDbContext`, and integrate Identity package into Infrastructure layer

---
+++ apps/Backend/Infrastructure/Data/Vgt7UserDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Infrastructure.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class Vgt7UserDbContext(DbContextOptions<Vgt7UserDbContext> options) : DbContext(options)
+{
+    public DbSet<Vgt7User> Users => Set<Vgt7User>();
+}
---
+++ apps/Backend/Infrastructure/Model/Vgt7User.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Model;
+
+public sealed class Vgt7User : IdentityUser
+{
+    private static readonly Regex EmailRegex = new(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.Compiled);
+
+    [AllowNull]
+    public override string UserName
+    {
+        get => base.UserName ?? string.Empty;
+        set => base.UserName = value;
+    }
+
+    [AllowNull]
+    public override string Email
+    {
+        get => base.Email ?? string.Empty;
+        set => base.Email = value;
+    }
+
+    /// <summary>
+    /// Конструктор без параметров для EF Core и UserManager
+    /// </summary>
+    // ReSharper disable once MemberCanBePrivate.Global
+    public Vgt7User() : base()
+    {
+    }
+
+    public Vgt7User(string email) : this()
+    {
+        ValidateEmailFormat(email);
+
+        Email = email;
+        NormalizedEmail = Email.ToLowerInvariant();
+
+        UserName = ExtractUsernameFromEmail(Email);
+    }
+
+    private static string ExtractUsernameFromEmail(string email)
+    {
+        var atIndex = email.IndexOf('@');
+        return atIndex > 0 ? email.Substring(0, atIndex) : email;
+    }
+
+    private static void ValidateEmailFormat(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            throw new ArgumentException("Email cannot be empty.", nameof(email));
+        }
+
+        if (!EmailRegex.IsMatch(email))
+        {
+            throw new ArgumentException("Invalid Email format.", nameof(email));
+        }
+    }
+}

da8d0a93b0a97f75593d881454ef07110038da7e
Олександр Кубарич <kuseas5@gmail.com>
2026-09-17 13:45:32 +0300

    test: add integration test project `Tests-API-NUnit` with base setup and custom API factory

---
+++ apps/Backend/Tests-API-NUnit/Infrastructure/CustomApiFactory.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Testcontainers.PostgreSql;
+
+namespace API_NUnit_Tests.Infrastructure;
+
+public class CustomApiFactory : WebApplicationFactory<Program>
+{
+    public readonly PostgreSqlContainer DbContainer = new PostgreSqlBuilder("timescale/timescaledb:latest-pg18")
+        .WithDatabase("test_db")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            builder.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+            });
+            
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+            
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(DbContainer.GetConnectionString()));
+        });
+    }
+}
---
+++ apps/Backend/Tests-API-NUnit/Infrastructure/IntegrationTestBase.cs
@@ -0,0 +1,42 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API_NUnit_Tests.Infrastructure;
+
+public abstract class IntegrationTestBase : IDisposable
+{
+    protected CustomApiFactory Factory;
+    protected HttpClient Client;
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        Factory = new CustomApiFactory();
+        await Factory.DbContainer.StartAsync();
+        
+        using var scope = Factory.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        Client = Factory.CreateClient();
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await Factory.DbContainer.StopAsync();
+        await Factory.DbContainer.DisposeAsync();
+        Dispose();
+    }
+
+    public void Dispose()
+    {
+        Factory.Dispose();
+        Client.Dispose();
+    }
+}

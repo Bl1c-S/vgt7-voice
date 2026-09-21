4e9656e4d6247289a063c843e421a96702d72a71
Олександр Кубарич <kuseas5@gmail.com>
2026-09-22 00:27:19 +0300

    fix(EmailAuthService): enhance error handling, improve password policies, and update configurations - Add logging for argument exceptions. - Improve duplicate email handling. - Enforce strong password policies in identity configuration. - Update authentication secret to meet minimum security standards.

--- apps/Backend/API/Services/Auth/EmailAuthService.cs
+++ apps/Backend/API/Services/Auth/EmailAuthService.cs
@@ -7,21 +7,23 @@ public class EmailAuthService(UserManager<Vgt7User> userManager)
 {
     public async Task<(bool wasCreated, Vgt7User? user)> Register(string email, string password)
     {
-        var user = await userManager.FindByEmailAsync(email);
-
-        if (user != null)
-            return (false, user);
-
         var newUser = new Vgt7User(email);
         var result = await userManager.CreateAsync(newUser, password);
 
         if (result.Succeeded)
             return (true, newUser);
-        
-        return (false, null);
+
+        if (result.Errors.Any(e => e.Code is "DuplicateUserName" or "DuplicateEmail"))
+        {
+            var user = await userManager.FindByEmailAsync(email);
+            if (user != null) return (false, user);
+        }
+
+        var errors = string.Join("\n", result.Errors.Select(e => e.Description));
+        throw new ArgumentException(errors);
     }
 
-    public async Task<(bool wasCreated, Vgt7User? user)> Login(string email, string password)
+    public async Task<(bool isLogin, Vgt7User? user)> Login(string email, string password)
     {
         var user = await userManager.FindByEmailAsync(email);
 
@@ -32,7 +34,7 @@ public class EmailAuthService(UserManager<Vgt7User> userManager)
 
         if (isPasswordCorrect)
             return (true, user);
-        
+
         return (false, null);
     }
 }
